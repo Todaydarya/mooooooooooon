03f6903b583057c8d9228d7395b5d6688e296dbe
Elick17 <ellinastark15@gmail.com>
2022-11-01 22:01:57 +0300

    moon_1

---
+++ js/jquery.scrollTo-min.js
@@ -0,0 +1,32 @@
+
+    // //no conflict jquery
+    // jQuery.noConflict();
+
+    // (function ($) {
+
+    //     //jquery code goes here.         
+    //     $.localScroll.defaults.axis = 'x';
+    //     $.localScroll();
+
+    // })(jQuery);
+
+
+    // (function ($) {
+
+    //     $(".slidingDiv").hide();
+    //     $(".show_hide").show();
+
+    //     $('.show_hide').click(function () {
+
+    //         $(".slidingDiv").slideToggle();
+    //     });
+
+    // })(jQuery);
+
+    
+    // $(document).ready(function () {
+    //     $.localScroll.defaults.axis = 'x';
+    //     $.localScroll();
+        
+    //     });
+
